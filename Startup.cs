dbd4c0228891363a14fe1440d06dea4534cd3040
Sherzod9808 <uralovsherzod03@gmail.com>
2022-02-17 21:09:34 +0500

    konstruktor

--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -12,8 +13,14 @@ namespace ExsampleMvc
 {
     public class Startup
     {
+        private readonly IConfiguration Configuree;
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public Startup(IConfiguration configuration)
+        {
+            this.Configuree = configuration;
+        }
         public void ConfigureServices(IServiceCollection services)
         {
         }
