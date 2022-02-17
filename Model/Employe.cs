25c9a5498eb10113629dd05369d9c96920a5984d
Sherzod9808 <uralovsherzod03@gmail.com>
2022-02-17 22:31:41 +0500

    Employye yaratildi

---
+++ Model/Employe.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExsampleMvc.Model
+{
+    public class Employe
+    {
+        public int EmployeID { get; set; }
+        public String FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
